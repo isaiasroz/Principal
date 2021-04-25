6c22cc418e4beca7f913e82997580f9d0a51b523
Isaías <Isaas@DESKTOP-89RA1U1>
2021-04-25 19:33:37 +0200

    Se añaden comentarios Javadoc.

--- DUAL-Actividad4-Entrega2/src/entrega2/Principal.java
+++ DUAL-Actividad4-Entrega2/src/entrega2/Principal.java
@@ -2,9 +2,17 @@ package entrega2;
 
 import java.util.Random;
 import java.util.Scanner;
-
+/**
+ * Clase Principal. Representa un array que se inicializa con números aleatorios.
+ * @author Isaías
+ * @version 1.1
+ * @since 1.0
+ */
 public class Principal {
-
+	/**
+	 * Método main, representa el punto de acceso del sistema para la ejecución del código.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		// Instancias.
@@ -24,7 +32,11 @@ public class Principal {
 		p.showArray(randomArray);
 		p.farewell(reader);
 	}
-
+	/**
+	 * Método que interrumpe el flujo de entrada y proporciona la salida del sistema. Como parámetro
+	 * se pasa la variable Scanner. No devuelve ningún valor.
+	 * @param reader. Variable de flujo de entrada.
+	 */
 	private void farewell(Scanner reader) {
 		
 		reader.close();
@@ -32,12 +44,23 @@ public class Principal {
 		System.out.println("Fin de la aplicaciÃ³n, hasta pronto.");
 		System.exit(0);
 	}
-
+	/**
+	 * Método que muestra por consola un array unidimensional de cualquier tammaño.
+	 * Como parámetro se pasa la referencia del array.
+	 * @param randomArray. Array a mostrar.
+	 */
 	private void showArray(int[] randomArray) {
 		
 		for(int i=0; i<randomArray.length; i++) System.out.print(randomArray[i]+" | ");
 	}
-
+	/**
+	 * Método que genera números aleatorios comprendidos dentro de un rango determinado.
+	 * No repite valores. Como parámetros se pasan la referencia del array y dos valores que
+	 * suponen los límites para los valores generados.
+	 * @param randomArray. Referencia del array.
+	 * @param MIN. Valor mínimo.
+	 * @param MAX. Valor máximo.
+	 */
 	private void numbersGenerator(int[] randomArray, int MIN, int MAX) {
 		
 		Random random = new Random();
